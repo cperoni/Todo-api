1cda4a548f3dde1eaadfed7278c5bdf12b0e2dd8
christianperoni <christian.peroni@gmail.com>
2019-09-20 11:45:20 +0200

    refactor API with underscore

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -13,20 +14,15 @@ app.use(bodyParser.json());
 app.get('/todos', function (req, res) {
     res.json(todos);
 })
+
 //GET /todos/:id
 app.get('/todos/:id', function (req, res) {
-
     var todoid = parseInt(req.params.id, 10);
-    var todoToRes;
 
-    todos.forEach(element => {
-        if (element.id == todoid) {
-            todoToRes = element;
-        }
-    });
+    var matchedTodo = _.findWhere(todos, { id: todoid });
 
-    if (todoToRes) {
-        res.json(todoToRes);
+    if (matchedTodo) {
+        res.json(matchedTodo);
     } else {
         res.status(404).send();
     }
@@ -34,7 +30,14 @@ app.get('/todos/:id', function (req, res) {
 
 //POST /todos
 app.post('/todos', function (req, res) {
-    var body = req.body;
+    // var body = req.body;
+    var body = _.pick(req.body, 'description', 'completed');
+
+    if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+        return res.status(400).send();
+    }
+
+    body.description = body.description.trim();
     todoNextId = todos.length + 1;
 
     body.id = todoNextId;

1236b389156a00e4f5b94aefbc3db9c4fd5965fd
christianperoni <christian.peroni@gmail.com>
2019-09-20 10:13:15 +0200

    add POST for todos

--- server.js
+++ server.js
@@ -1,28 +1,17 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-var todos = [{
-    id: 1,
-    description: 'meet mom for lunch',
-    completed: true
-}, {
-    id: 2,
-    description: 'go to marker',
-    completed: false
-}, {
-    id: 3,
-    description: 'make a cake',
-    completed: true
-}]
+var todos = [];
+var todoNextId = 1;
 
+app.use(bodyParser.json());
 
 //GET /todos
 app.get('/todos', function (req, res) {
-
     res.json(todos);
-
 })
 //GET /todos/:id
 app.get('/todos/:id', function (req, res) {
@@ -41,14 +30,23 @@ app.get('/todos/:id', function (req, res) {
     } else {
         res.status(404).send();
     }
+});
 
-})
+//POST /todos
+app.post('/todos', function (req, res) {
+    var body = req.body;
+    todoNextId = todos.length + 1;
+
+    body.id = todoNextId;
+    todos.push(body);
+
+    res.json(body);
+});
 
 app.get('/', function (req, res) {
-
     res.send('Todo API Root');
 });
 
 app.listen(PORT, function () {
     console.log('Express listening on port ' + PORT);
-})
+});

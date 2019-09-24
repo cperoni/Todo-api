40b51570148603ed1a3b0d2e946ace9036fc8908
christianperoni <christian.peroni@gmail.com>
2019-09-24 12:24:14 +0200

    Add todo Models and post Todo

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
+var db = require('./db.js');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -47,17 +48,24 @@ app.get('/todos/:id', function (req, res) {
 app.post('/todos', function (req, res) {
     var body = _.pick(req.body, 'description', 'completed');
 
-    if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-        return res.status(400).send();
-    }
 
-    body.description = body.description.trim();
-    todoNextId = todos.length + 1;
+    db.todo.create(body).then(function (todo) {
+        res.json(todo.toJSON());
+    }, function (e) {
+        res.status(400).json(e);
+    });
 
-    body.id = todoNextId;
-    todos.push(body);
+    // if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+    //     return res.status(400).send();
+    // }
 
-    res.json(body);
+    // body.description = body.description.trim();
+    // todoNextId = todos.length + 1;
+
+    // body.id = todoNextId;
+    // todos.push(body);
+
+    // res.json(body);
 });
 
 //DELETE /todos/:id
@@ -107,6 +115,8 @@ app.get('/', function (req, res) {
     res.send('Todo API Root');
 });
 
-app.listen(PORT, function () {
-    console.log('Express listening on port ' + PORT);
+db.sequelize.sync().then(function () {
+    app.listen(PORT, function () {
+        console.log('Express listening on port ' + PORT);
+    });
 });
